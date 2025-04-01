aeb215f4c24ac208bdba5fd3d11a809bd64a6f7b
Nick Potamianos <up1084537@ac.upatras.gr>
2025-04-01 15:07:58 +0300

    warning fix

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -89,8 +89,8 @@ const Footer = () => {
                   className="text-white hover:text-[var(--terracotta)] transition-colors duration-300 text-sm font-medium flex items-center"
                 >
                   Book your stay
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
                   </svg>
                 </a>
               </div>
